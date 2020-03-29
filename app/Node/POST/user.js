c28426ec9aee8a0727c2bd33799335421f7f8bdc
Enrico <enrico.schifano@secondhandmobile.it>
2020-03-29 15:29:51 +0200

    #5 little fixes

--- app/Node/POST/user.js
+++ app/Node/POST/user.js
@@ -4,46 +4,86 @@ const dbManager = require("../../Globals/dbManager.js");
 module.exports = async function(request, response) {
 	let success = false;
 	let error = "";
+
 	try {
 		let nick = request.body.nickname;
 		let cards = [];
 		let isMaster = false;
 
-		if (!nick || nick === ""){
-		throw "Nickname non inserito";
+		if (!nick || nick === "") {
+			throw "Nickname non inserito";
 		}
 		await dbManager.connect();
-		cards = await dbManager.models.cards.select({isBlack:false});
-		cards.sort(function(a,b){
-			return 0.5 - Math.random()
-		});
-		console.log(cards);
 
 		//await dbManager.models.users.destroy({}); //PER RIPULIRE IL DATABASE IN FASE DI TEST
- 	 	let checkMaster = await dbManager.models.users.select({});
- 		if(checkMaster.length === 0){
+		let checkMaster = await dbManager.models.users.select({}, ["nickname"]);
+		if (checkMaster.length === 0) {
 			isMaster = true;
+		}
+
+		if (isMaster) {
+			let whiteDeck = await dbManager.models.cards.select({
+				isBlack: false
+			}).map(function(item) {
+				return item.uuid;
+			});
+			let blackDeck = await dbManager.models.cards.select({
+				isBlack: true
+			}).map(function(item) {
+				return item.uuid;
+			});
+			whiteDeck.sort(function() {
+				return 0.5 - Math.random();
+			});
+			blackDeck.sort(function() {
+				return 0.5 - Math.random();
+			});
+			cards = whiteDeck.splice(0, 10);
+			//console.log(cards);
+			await dbManager.models.games.delete({});
+			await dbManager.models.games.create({
+				whiteDeck: whiteDeck,
+				blackDeck: blackDeck,
+				isStarted: false,
+				isEnded: false,
+				turnWinner: null,
+				turnWinnerCards: null,
+				currentBlackCard: null
+			});
+		} else {
+			let games = await dbManager.models.games.select({});
+			if (!games.length) {
+				throw "Errore, gioco non trovato";
+			}
+			let whiteDeck = games[0].whiteDeck;
+			cards = whiteDeck.splice(0, 10);
+			await dbManager.models.games.modify({}, {
+				whiteDeck: whiteDeck
+			});
 		}
 
 		await dbManager.models.users.create({
 			nickname: nick,
-			cards: cards.slice(0,10),
+			cards: cards,
 			points: 0,
-			isMaster: false,
+			isMaster: isMaster,
+			response: null
 		});
-		console.log(await dbManager.models.users.select({}));
-		console.log(isMaster);
+
+
+		//console.log(await dbManager.models.users.select({}));
+		//console.log(isMaster);
 		await dbManager.close();
 		success = true;
 
 	} catch (err) {
-			console.log(err);
-			dbManager.close();
-			error = err;
-		}
+		console.log(err);
+		dbManager.close();
+		error = err;
+	}
 
-		response.status(200).send({
-			success: success,
-			error: error
-		});
+	response.status(200).send({
+		success: success,
+		error: error
+	});
 };
