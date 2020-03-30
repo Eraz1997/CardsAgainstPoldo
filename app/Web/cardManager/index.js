bf1ee59e1c63526790aed344fbcad4a942c34b8d
Mattia Brozzo <mbrozzo@gmail.com>
2020-03-30 02:05:55 +0200

    #29 created and tested text file deck loader

--- app/Web/cardManager/index.js
+++ app/Web/cardManager/index.js
@@ -45,7 +45,6 @@ angular.module("app", [])
 			await sendCard(card);
 		};
 
-
 		$scope.deleteCard = async function(card) {
 			try {
 				await $http.delete("/api/card/?uuid=" + card.uuid);
@@ -55,4 +54,61 @@ angular.module("app", [])
 			}
 		};
 
+    $scope.sendDeck = async function(deck){
+			deck=deck.replace(/^\s*[\r\n]/gm,"\n");
+			let cards = deck.match(/[^\r\n]+/g);
+			if (cards != null && cards.length) {
+				let blackCards = false;
+				if (cards[0] === "BIANCHE" || cards[0] === "NERE") {
+					for (var card of cards) {
+						if (card === "BIANCHE") {
+							blackCards = false;
+							console.log("Sezione carte bianche");
+						}
+						else if (card === "NERE") {
+							blackCards = true;
+							console.log("Sezione carte nere");
+						}
+						else {
+							if (blackCards && !card.includes("_")) {
+								$window.alert("La seguente carta non è stata inviata poiché non contiene nessuno spazio vuoto:\n" + card);
+							}
+							else {
+								await sendCard({text: card, isBlack: blackCards});
+								console.log("Carta inviata: " + card);
+							}
+						}
+					}
+				}
+				else {
+					$window.alert("Formato deck non valido");
+				}
+			}
+			else {
+				$window.alert("Deck vuoto");
+			}
+		};
+
+	})
+
+	.directive('onReadFile', function ($parse) {
+		return {
+			restrict: 'A',
+			scope: false,
+			link: function(scope, element, attrs) {
+	            var fn = $parse(attrs.onReadFile);
+
+				element.on('change', function(onChangeEvent) {
+					var reader = new FileReader();
+
+					reader.onload = function(onLoadEvent) {
+						scope.$apply(function() {
+							fn(scope, {$fileContent:onLoadEvent.target.result});
+						});
+					};
+
+					reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+				});
+			}
+		};
 	});
